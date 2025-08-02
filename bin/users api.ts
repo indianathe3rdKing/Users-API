3edff91c92cd4e92381212599c070722e3ce080c
indianathe3rd <indianathe3rd@gmail.com>
2025-08-02 13:37:03 +0200

    env

--- bin/users api.ts
+++ bin/users api.ts
@@ -2,7 +2,11 @@
 import * as cdk from "aws-cdk-lib";
 import { UsersApiStack } from "../lib/users-api-stack";
 import { DynamoDBStack } from "../lib/dynamodb-stack";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const API_BASE_URL = process.env.API_BASE_URL;
 const app = new cdk.App();
 
 const dynamodbStack = new DynamoDBStack(app, "DynamoDBStack");
